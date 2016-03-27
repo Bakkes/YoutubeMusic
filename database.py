f777b0e3818534f77152ab93ef1eddedba527048
Chris <chris.m@live.nl>
2016-03-28 01:18:00 +0200

    Database now with path stuff

--- database.py
+++ database.py
@@ -7,12 +7,13 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean, TIMESTAM
 from sqlalchemy.orm import relationship, Session
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.session import sessionmaker
-
+import os
+from config import CURRENT_PATH
 class Source():
     youtube = 0
     spotify = 1
 
-engine = create_engine('sqlite:///database.db', echo=False)
+engine = create_engine('sqlite:///%s' % os.path.join(CURRENT_PATH, "database.db"), echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
